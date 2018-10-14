b43ac5a10b572d7955aada6b52ae31245b541359
SedMek <32764135+SedMek@users.noreply.github.com>
2018-10-14 21:45:10 +0200

    front & ourTMDB
    I have updated almost all the files, Added flask architecture and front.

---
+++ our_tmdb.py
@@ -0,0 +1,51 @@
+import requests
+from pprint import pprint
+
+api_key = "f3e96aa12213aa6d0925d98470ba6fec"
+api_version = "3"
+
+
+class Serie:
+    def __init__(self, id):
+        self.id = id
+        self.serie_infos = requests.get('https://api.themoviedb.org/' + api_version + '/tv/' + str(
+            id) + '?api_key=' + api_key + '&language=en-US').json()
+
+
+class Season(Serie):
+    def __init__(self, id, season_number):
+        Serie.__init__(self, id)
+        self.season_number = season_number
+        self.season_infos = requests.get(
+            'https://api.themoviedb.org/' + api_version + '/tv/' + str(id) + '/season/' + str(
+                season_number) + '?api_key=' + api_key + '&language=en-US').json()
+
+
+class Episode(Season):
+    def __init__(self, id, season_number, episode_number):
+        Season.__init__(self, id, season_number)
+        self.episode_number = episode_number
+        self.episode_infos = requests.get(
+            'https://api.themoviedb.org/' + api_version + '/tv/' + str(id) + '/season/' + str(
+                season_number) + '/episode/' + str(episode_number) + '?api_key=' + api_key + '&language=en-US').json()
+
+
+class Search:
+    def __init__(self, query, language="en-US"):
+        self.query = query
+        self.language = language
+        self.basic_search_url = "http://api.themoviedb.org/" + api_version + "/search/tv?api_key=" + api_key + "&query=" + query + "&language" + self.language
+        self.resp = requests.get(self.basic_search_url).json()
+        self.total_pages = self.resp["total_pages"]
+
+    def get_page(self, page=1):
+        return requests.get(self.basic_search_url + "&page=" + str(page)).json()
+
+
+def main():
+    got = Serie(1399)
+    pprint(got.serie_infos)
+
+
+if __name__ == "__main__":
+    main()
---
+++ vues.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, url_for, redirect
+import our_tmdb
+
+app = Flask(__name__)
+app.secret_key = "hello".encode()
+
+POSTER_PATH = "https://image.tmdb.org/t/p/w200/"
+# this variable should be global so the user can remember the series that he has already added
+fav_series = ["1399", "1396", "1100", "1668", "71446"]
+
+
+def set_fav_posters(list_of_series):
+    """
+    takes a list of series (Id) and returns a list of their posters
+    """
+    posters = []
+    for i in range(len(list_of_series)):
+        posters.append(POSTER_PATH + our_tmdb.Serie(list_of_series[i]).serie_infos[
+            "poster_path"])
+    return posters
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    search_result_code = 0 # 0 for no search issued, 1 for search with results and -1 for search without results
+    id_poster = dict()
+    if request.method == "POST":
+        # fav_series.insert(0, request.form["search"])  # to add the new series in first position
+        search_result = our_tmdb.Search(request.form["search"]).get_page()["results"]
+        if len(search_result) == 0:
+            search_result_code = -1
+        else:
+            search_result_code = 1
+            for series in search_result:
+                try:
+                    id_poster[series["id"]] = POSTER_PATH + series["poster_path"]
+                except: #some series might not have posters
+                    pass
+    if request.method == "GET":
+        pass  # TODO
+    posters = set_fav_posters(fav_series)
+    return render_template("index.html", posters=posters, id_poster=id_poster, search_result_code= search_result_code)
+
+
+@app.route("/addSeries/<int:series_id>")
+def add_series_to_fav(series_id):
+    fav_series.insert(0, series_id)
+    return redirect(url_for("home"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
