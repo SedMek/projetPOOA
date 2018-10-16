bc054a16806d72eec36af8a346052d2c18175fe0
SedMek <32764135+SedMek@users.noreply.github.com>
2018-10-16 12:19:23 +0200

    added fav series storage in local file and added a route to clear this fav series list

--- vues.py
+++ vues.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request, url_for, redirect
 import our_tmdb
+import pickle
 
 app = Flask(__name__)
 app.secret_key = "hello".encode()
 
 POSTER_PATH = "https://image.tmdb.org/t/p/w200/"
+
+
 # this variable should be global so the user can remember the series that he has already added
-fav_series = ["1399", "1396", "1100", "1668", "71446"]
+
+
+def get_fav_object():
+    with open('static/fav', 'rb') as fav:
+        my_unpickler = pickle.Unpickler(fav)
+        fav_list = my_unpickler.load()
+        return fav_list
+
+
+def save_fav_object(fav_series):
+    with open('static/fav', 'wb') as fav:
+        my_pickler = pickle.Pickler(fav)
+        my_pickler.dump(fav_series)
 
 
 def set_fav_posters(list_of_series):
@@ -22,7 +37,8 @@ def set_fav_posters(list_of_series):
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    search_result_code = 0 # 0 for no search issued, 1 for search with results and -1 for search without results
+    fav_series = get_fav_object()
+    search_result_code = 0  # 0 for no search issued, 1 for search with results and -1 for search without results
     id_poster = dict()
     if request.method == "POST":
         # fav_series.insert(0, request.form["search"])  # to add the new series in first position
@@ -34,19 +50,32 @@ def home():
             for series in search_result:
                 try:
                     id_poster[series["id"]] = POSTER_PATH + series["poster_path"]
-                except: #some series might not have posters
+                except:  # some series might not have posters
                     pass
     if request.method == "GET":
         pass  # TODO
+
     posters = set_fav_posters(fav_series)
-    return render_template("index.html", posters=posters, id_poster=id_poster, search_result_code= search_result_code)
+    return render_template("index.html", posters=posters, id_poster=id_poster, search_result_code=search_result_code)
 
 
 @app.route("/addSeries/<int:series_id>")
 def add_series_to_fav(series_id):
+    fav_series = get_fav_object()
+
+    if series_id in fav_series:  # if the series is already in the list, remove it from the list
+        fav_series.remove(series_id)
+
     fav_series.insert(0, series_id)
+    save_fav_object(fav_series)
+    return redirect(url_for("home"))
+
+
+@app.route("/clear_fav")
+def clear_fav():
+    save_fav_object([])
     return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
