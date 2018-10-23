8f000fa1ff7310ffaca42f69f6a2bd96167b8b72
noubair.m@gmail.com <noubair.m@gmail.com>
2018-10-23 14:53:53 +0200

    Ajout script notifications

--- our_tmdb.py
+++ our_tmdb.py
@@ -3,21 +3,23 @@ from pprint import pprint
 
 api_key = "f3e96aa12213aa6d0925d98470ba6fec"
 api_version = "3"
+POSTER_PATH="https://image.tmdb.org/t/p/w200/"
 
-
-class Serie:
+class Series:
     def __init__(self, id):
         self.id = id
-        self.serie_infos = requests.get('https://api.themoviedb.org/' + api_version + '/tv/' + str(
+        self.series_info = requests.get('https://api.themoviedb.org/' + api_version + '/tv/' + str(
             id) + '?api_key=' + api_key + '&language=en-US').json()
         
 
+        
 
-class Season(Serie):
+
+class Season(Series):
     def __init__(self, id, season_number):
-        Serie.__init__(self, id)
+        Series.__init__(self, id)
         self.season_number = season_number
-        self.season_infos = requests.get(
+        self.season_info = requests.get(
             'https://api.themoviedb.org/' + api_version + '/tv/' + str(id) + '/season/' + str(
                 season_number) + '?api_key=' + api_key + '&language=en-US').json()
 
@@ -26,7 +28,7 @@ class Episode(Season):
     def __init__(self, id, season_number, episode_number):
         Season.__init__(self, id, season_number)
         self.episode_number = episode_number
-        self.episode_infos = requests.get(
+        self.episode_info = requests.get(
             'https://api.themoviedb.org/' + api_version + '/tv/' + str(id) + '/season/' + str(
                 season_number) + '/episode/' + str(episode_number) + '?api_key=' + api_key + '&language=en-US').json()
 
@@ -38,7 +40,7 @@ class Search:
         self.basic_search_url = "http://api.themoviedb.org/" + api_version + "/search/tv?api_key=" + api_key + "&query=" + query + "&language" + self.language
         self.resp = requests.get(self.basic_search_url).json()
         self.total_pages = self.resp["total_pages"]
-        self.series=[Serie(e["id"]) for e in self.get_page(self)["results"]]
+        self.series=[Series(e["id"]) for e in self.get_page(self)["results"]]
     
     
     def get_page(self, page=1):
@@ -48,8 +50,8 @@ class Search:
 
 
 def main():
-    got = Serie(1399)
-    # pprint(got.serie_infos)
+    got = Series(1399)
+    # pprint(got.series_info)
 
 
 if __name__ == "__main__":
---
+++ run_notification.py
@@ -0,0 +1,6 @@
+import time
+import datetime
+while 1:
+    runfile('/home/insight/pooa/Local-projet-POOA/projetpooa/venv/observer_pattern.py', wdir='/home/insight/pooa/Local-projet-POOA/projetpooa/venv')
+    print(str(datetime.datetime.now().hour)+":"+str(datetime.datetime.now().minute)+":"+str(datetime.datetime.now().second))
+    time.sleep(0.5)
