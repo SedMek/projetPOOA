c4afdf9ca4a9a725fcdb408acf2f1178e508aba0
SedMek <32764135+SedMek@users.noreply.github.com>
2018-10-27 00:11:42 +0200

    still fixing merge issues

--- static/seddikMade.js
+++ static/seddikMade.js
@@ -19,12 +19,18 @@ alert(stringData);
 $.getJSON("/storage", function (data) {result = data});
 */
 
-function show_info_by_id(series_id) {
-    let series = result[series_id];
+function show_series_info(series) {
     let networks = "";
     for (var i = 0; i < series.networks.length; i++) {
-        networks = networks + series.networks[i].name;
+        networks = series.networks[i].name + " & " + networks;
     };
+    networks = networks.substring(0, networks.length-3);
+
+    let genres = "";
+    for (var i = 0; i < series.genres.length; i++) {
+        genres = series.genres[i].name + " & " + genres;
+    };
+    genres = genres.substring(0, genres.length-3);
 
     let remove_link = $("#remove-link").attr("href");
     remove_link = remove_link.substring(0, remove_link.lastIndexOf("/")+1) + series.id;
@@ -32,7 +38,7 @@ function show_info_by_id(series_id) {
     $("#poster").attr("src", POSTER_PATH + series.poster_path);
     $("#title").text(series.name);
     $("#networks").text(networks);
-
+    $("#genres").text(genres);
     $("#overview").text(series.overview);
     $("#remove-link").attr("href", remove_link);
 
