29ac916e05cb557b08663b3a2e84765bbff329bc
SedMek <32764135+SedMek@users.noreply.github.com>
2018-10-31 00:51:33 +0100

    added total view time and seasons display

--- static/seddikMade.js
+++ static/seddikMade.js
@@ -16,7 +16,8 @@ alert(stringData);
 $.getJSON("/storage", function (data) {result = data});
 */
 
-function show_series_info(series) {
+function fill_series_info(series) {
+    /* presentation vue */
     let networks = "";
     for (var i = 0; i < series.networks.length; i++) {
         networks = series.networks[i].name + " & " + networks;
@@ -39,5 +40,71 @@ function show_series_info(series) {
     $("#overview").text(series.overview);
     $("#remove-link").attr("href", remove_link);
 
+    /* seasons vue */
+    $("#mySidenav").empty();
+    for (var i = 0; i < series.seasons.length; i++) {
+        let season = $("<a>", {href: "#", onclick: "show_season_info("+ JSON.stringify(series.seasons[i]) +")"} );
+        season.text("Season "+i);
+        $("#mySidenav").append(season);
+    };
+}
+
+function fill_season_info(season) {
+    $("#season_poster").attr("src", POSTER_PATH + season.poster_path);
+    $("#season_title").text(season.name);
+    if (season.air_date){
+        $("#season_air_date").text(season.air_date);
+    }else {
+        $("#season_air_date").text('To be announced');
+    }
+    if (season.overview){
+        $("#season_overview").text(season.overview);
+    }else {
+        $("#season_overview").text("No overview has been announced");
+    }
+
+}
+
+function show_series_info(series) {
+    fill_series_info(series);
+    $("#main").hide();
+    $("#presentation").show();
     $(".series_info_block").show();
+}
+
+function show_season_info(season) {
+    fill_season_info(season);
+    $("#main").show();
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function display_seasons() {
+    $("#presentation").hide();
+    /* $("#main").show(); */
+    openNav();
+};
+
+function display_presentation() {
+    closeNav();
+    $("#main").hide();
+    $("#presentation").show();
+
+}
+
+function hide_info_block() {
+    display_presentation();
+    $("#presentation").hide();
+    $("#main").hide();
+    $(".series_info_block").hide();
 }
